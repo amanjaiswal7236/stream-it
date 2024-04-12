111ceab7788cd939559140a374fc86daeb0cc19f
AMAN JAISWAL <124071449+amanjaiswal7236@users.noreply.github.com>
2024-04-13 01:03:52 +0530

    connected sql database with prisma

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({});
+
+export const config = {
+  matcher: [
+    "/((?!\\.(?:js|css|json|xml|rss|png|jpg|jpeg|gif|svg|woff|woff2|ico|txt|xml|pdf|zip|gz|map|mp3|mp4|csv|ts|tsx)$|_next).*)",
+    "/", 
+    "/(api|trpc)(.*)"
+  ],
+};
