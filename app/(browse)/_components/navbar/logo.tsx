b414fb82c50c6885c66a4f688a4c2fb7533723ba
AMAN JAISWAL <124071449+amanjaiswal7236@users.noreply.github.com>
2024-04-18 21:48:49 +0530

    added navbar

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -1,54 +1,36 @@
 import Image from "next/image";
-import { Poppins } from "next/font/google";
+import Link from 'next/link';
 import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
 
 const font = Poppins({
-    subsets: ["latin"],
-    weight: ['400', '700'],
-});
+    subsets: ['latin'],
+    weight: ['200', '400', '600', '700'],
+})
 
 export const Logo = () => {
-    const containerStyle = {
-        background: 'linear-gradient(135deg, #9B1111, #FF1A1A)', // Gradient background from darker red to brighter red
-        padding: '16px', // Padding around the logo
-        boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)', // Shadow for a subtle effect
-        transition: 'transform 0.3s ease', // Smooth transition for hover effect
-        borderRadius: '8px', // Rounded corners
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        gap: '8px',
-        marginTop: '32px', // Increase margin from the top
-    };
-
-    const hoverStyle = {
-        transform: 'translateY(-2px)', // Lift the container on hover
-    };
+    
 
     return (
-        <div 
-            className={cn(
-                "text-white",
-                "mb-6"
-            )}
-            style={containerStyle as React.CSSProperties} // Explicitly specify the type of containerStyle
-        >
-            <Image
-                src="/smooth.svg"
-                alt="Stream It"
-                width={48} // Decreased size
-                height={48} // Decreased size
-            />
-            <span 
-                className={cn(
-                    "font-sans",
-                    "text-lg font-bold",
-                )}
-                style={hoverStyle}
-            >
-                Stream It
-            </span>
-        </div>
+        <Link href='/'>
+            <div className="flex items-center gap-x-4 hover:opacity-75 transition">
+                <div className="bg-red-600 rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
+                    <Image
+                        src="/smooth.svg"
+                        alt="Smooth Logo"
+                        width={32}
+                        height={32}
+                    />
+                </div>
+                <div className={cn(
+                    "hidden lg:flex flex-col gap-y-1 text-primary-foreground",
+                    font.className)}>
+                    <p className="text-lg font-semibold">Stream It</p>
+                    <p className="text-xs text-muted-foreground">
+                        Let&apos;s watch something
+                    </p>
+                </div>
+            </div>
+        </Link>
     );
 };
