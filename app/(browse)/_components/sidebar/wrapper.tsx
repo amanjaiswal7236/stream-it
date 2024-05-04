6b00869d9786bfd08f55270544ff3f85e04800ac
AMAN JAISWAL <124071449+amanjaiswal7236@users.noreply.github.com>
2024-05-05 00:17:18 +0530

    added recommended list

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,6 +2,11 @@
 
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { useIsClient } from "usehooks-ts";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+
 
 interface WrapperProps {
     children: React.ReactNode;
@@ -10,7 +15,21 @@ interface WrapperProps {
 export const Wrapper = ({
     children,
 }: WrapperProps) => {
+    // const [isClient, setIsClient] = useState(false);
+    const isClient = useIsClient();
     const {collapsed} = useSidebar((state)=>state);
+
+    // useEffect(()=>{
+    //     setIsClient(true);
+    // },[]);
+
+    if(!isClient) return (
+        <aside className="fixed left-0 flex flex-col w-[70-px] lg:w-60 h-full bg-background border-r border-[#202E35] z-50">
+            <ToggleSkeleton/>
+            <RecommendedSkeleton/>
+        </aside>
+    
+    );
     return (
         <aside className={cn("fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#202E35] z-50", collapsed && "w-[70px]")}>
             {children}
